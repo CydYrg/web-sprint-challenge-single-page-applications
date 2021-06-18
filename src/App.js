8c5bead783a136c2265490671be17d1e89c459db
Ceyda Yurga <ceydayurga@Ceydas-MacBook-Air.local>
2021-06-18 15:35:34 +0100

    worked on App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
+
+import Home from './Home'
+import Item from './Item'
+import Pizza from './Pizza'
+
+import data from './data'
+
+function fetchStock() {
+  // fetchStock simulates getting data through axios.get(<URL>)
+  return Promise.resolve({ success: true, data })
+}
 
 const App = () => {
+  const [stock,
+    setStock] = useState([])
+
+  useEffect(() => {
+    fetchStock().then(res => setStock(res.data)
+    )
+  },
+  )
   return (
     <>
       <h1>Lambda Eats</h1>
@@ -8,4 +28,24 @@ const App = () => {
     </>
   );
 };
+<div className='App'>
+<nav>
+  <h1 className='store-header'>Lambda Eats</h1>
+  <div className='nav-links'>
+    <Link to='/'>Home</Link>
+    <Link to='/items-list'>Help</Link>
+  </div>
+</nav>
+
+<Switch>
+  <Route exact path="/items-list/:itemID">
+    <Item items={stock} />
+  </Route>
+  <Route exact path='/order-pizza' component={Pizza} />
+  <Route exact path='/' component={Home} />
+
+</Switch>
+
+</div> 
+
 export default App;
