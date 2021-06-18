163f5f378e96d6adb381d2d166c38a60fa135813
Ceyda Yurga <ceydayurga@Ceydas-MacBook-Air.local>
2021-06-18 20:05:13 +0100

    pizzaSchema added

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ const App = () => {
     setStock] = useState([])
 
   useEffect(() => {
-    fetchStock().then(res => setStock(res.data)
+    fetchStock().then(res => setStock(res./*data*/)
     )
   },
   )
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>LETS HAVE SOME PIZZA</p>
     </>
   );
 };
